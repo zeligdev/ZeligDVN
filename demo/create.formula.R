3d5a8d2f0dde3b90178d0890f2497400e4a279ba
Matt <mattisbusiness@gmail.com>
2011-10-25 17:06:10 -0400

    Adding ways to generate formula as strings

--- demo/create.formula.R
+++ demo/create.formula.R
@@ -1,1 +1,16 @@
 # ...
+
+library(ZeligDVN)
+
+explanatory <- c()
+outcome <- c()
+
+# outcome ~ explanatory
+univariate.formula <- function (outcome, explanatory) {
+  update(y ~ x, paste(outcome, "~", paste(explanatory, collapse=" + ")))
+}
+
+# cbind(outcome1, outcome2) ~ explantory
+# list(outcome1, outcome2) ~ explanatory
+multivariate.repeated.formula <- function (outcome, explanatory) {
+}
