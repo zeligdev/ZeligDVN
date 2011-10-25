3ffae247fbc88adb7f6a8f5cdbba18d84fc3d4e5
Matt <mattisbusiness@gmail.com>
2011-10-25 16:36:04 -0400

    Updating DESCRIPTION and NAMESPACE and File Remove

--- R/line.parser.R
+++
@@ -1,8 +0,0 @@
-# 
-eqn.keys <- list(
-                 "min (?:number )?equations" = "^\\s*(?:[0-9]+|-Inf)\\s$",
-                 "max (?:number )?equations" = "^\\s*(?:[0-9]+|Inf)\\s$",
-                 ""
-                 )
-
-
