091b46787249fe1362cdbfb7184873f29b2082e9
Matt <mattisbusiness@gmail.com>
2011-12-13 10:21:06 -0500

    info.matrix

--- R/info.matrix.R
+++ R/info.matrix.R
@@ -3,13 +3,13 @@
 #' This method facilitates the extraction of meaningful information from a
 #' an atomic *matrix* extracted from a \code{formula} and \code{data.frame}.
 #' @note This function is primarily used internally by the various 
-#'   \code{analyze} methods.
+#' \code{analyze} methods.
 #' @param x a matrix containing atomic values
 #' @param ... ignored parameters
 #' @param threshold an integer specifying the maximum size of allowing numeric
-#'   data to be considered potential factor data. That is, if a data.frame
-#'   contains a numeric column with less unique elements than the value set by
-#'   'threshold', then this method will consider it valid factor-style data.
+#' data to be considered potential factor data. That is, if a data.frame
+#' contains a numeric column with less unique elements than the value set by
+#' 'threshold', then this method will consider it valid factor-style data.
 #' @return an \code{info.atomic} object
 #' @author Matt Owen \email{mowen@@iq.harvard.edu}
 info.matrix <- function(x, ..., threshold) {
